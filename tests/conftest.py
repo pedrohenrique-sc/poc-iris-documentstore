7e0b71039d232cd54001cdca3e652f7868cc3031
Pedro Henrique <pedro@s-c.ai>
2026-04-09 16:46:39 -0300

    test: creating unit tests for intersystems-iris-documentstore

---
+++ tests/conftest.py
@@ -0,0 +1,26 @@
+# SPDX-FileCopyrightText: 2026-present Pedro Henrique <pedro@s-c.ai>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+import pytest
+from haystack.utils import Secret
+from haystack_integrations.document_stores.intersystems_iris import IRISDocumentStore
+
+@pytest.fixture()
+def document_store():
+    store = IRISDocumentStore(
+        connection_string=Secret.from_token(os.getenv("IRIS_CONNECTION_STRING", "localhost:1972/USER")),
+        username=Secret.from_token(os.getenv("IRIS_USERNAME", "_system")),
+        password=Secret.from_token(os.getenv("IRIS_PASSWORD", "SYS)")),
+        table_name="HaystackTest768", 
+        embedding_dim=768,
+    )
+    # Clean before test
+    ids = [d.id for d in store.filter_documents()]
+    store.delete_documents(ids)
+    yield store
+    # Clean after test
+    ids = [d.id for d in store.filter_documents()]
+    store.delete_documents(ids)
+    store.close()
